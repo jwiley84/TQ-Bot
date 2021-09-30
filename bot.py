6f84e446e7bcd6ace6cb648a2fe4254808064e10
Jessica Wailes <jess.m.wailes@gmail.com>
2021-09-30 10:10:30 -0700

    first commit

---
+++ bot.py
@@ -0,0 +1,54 @@
+from itertools import chain
+import os
+import sys
+from dotenv import load_dotenv
+from twitchio.ext import commands
+import irc.bot
+import requests
+
+class TwitchQuizBot(irc.bot.SingleServerIRCBot):
+    def  __init__(self, username, client_id, token, channel):
+        self.client_id = client_id
+        self.token = token
+        self.channel = '#' + channel
+
+
+        #get the channel id, needed for v5 api calls
+        url = 'https://api.twitch.tv/kraken/users?login' + channel
+        headers =  {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        r = requests.get(url, headers=headers).json()
+        self.channel_id = r['users'][0]['_id']
+
+        #create the IRC connection
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        print('Connecting to ') + server + ' on port ' + str(port) + '...'
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    load_dotenv()
+
+    username = os.getenv('USERNAME')
+    token=os.getenv('TOKEN')
+    client_id=os.getenv('CLIENT_ID')
+    prefix=os.getenv('BOT_PREFIX')
+    channel=os.getenv('CHANNEL')
+
+    bot = TwitchQuizBot(username, client_id, token, channel)
+    bot.start()
+
+
+
+
+if __name__ == "__main__":
+    main()
